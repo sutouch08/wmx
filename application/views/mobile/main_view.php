5bec359fb3280a2abd6fdff784e0364c13e7faf0
sutouch08 <sutouch08@gmail.com>
2025-11-07 20:49:07 +0700

    chang size of menu on mobile

--- application/views/mobile/main_view.php
+++ application/views/mobile/main_view.php
@@ -16,9 +16,11 @@
 	<div class="row">
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-5 text-center">
 			<h1>Hello! <?php echo get_cookie('displayName'); ?></h1>
-			<h5>Good to see you here</h5>
 		</div>
 		<div class="divider-hidden"></div>
+		<div class="col-xs-12 padding-5 text-center">
+			<button type="button" class="btn btn-white btn-primary" onclick="goTo('main')"><i class="fa fa-desktop"></i>&nbsp; Desktop Version</button>
+		</div>
 		<div class="divider"></div>
 	</div>
 	<div class="row">
